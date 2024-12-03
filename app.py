191bbccc021344241c04206133a39a722bc20b2a
eringilliland99 <erin.b.gilli@gmail.com>
2024-12-02 20:06:55 -0500

    Update app.py
    Added string ending

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ from werkzeug.utils import secure_filename
 ## Create instance of Flash class (WSGI application)
 # Add app configurations
 UPLOAD_FOLDER_DOCS = '/home/ec2-user/FinalProject_AdvancedPython/uploads/docs'
-UPLOAD_FOLDER_IMAGES = '/home/ec2-user/FinalProject_AdvancedPython/uploads/images
+UPLOAD_FOLDER_IMAGES = '/home/ec2-user/FinalProject_AdvancedPython/uploads/images'
 '
 ALLOWED_EXTENSIONS= {'pdf', 'docx', 'txt'}
 ALLOWED_EXTENSIONS_IMAGES = {'png', 'jpg', 'jpeg', 'gif'}
